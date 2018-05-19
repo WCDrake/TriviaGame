fda9fb2a7f8e59bf15cc8c6b896e9e110428432b
unknown <wconnordrake@gmail.com>
2018-05-19 17:15:47 -0400

    Progress- Need to do Questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,79 @@
-/*
-1) Transition to end screen
-2) Transition back
-Just do the transitions
-Questions on the back
-    - there will be an easy fix
-*/
-var timeCount= 30;
-var correct= 0;
-var wrong = 0;
-
-
-
+/*When the game resets, the timer increases. It also starts a few seconds in */
+var timeCount = 30;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
 $('.questionsAndTimer').hide();
+$('.endScreen').hide();
+
+$(".startGame").on("click", function () {
+    $(".questionsAndTimer").show();
+
+    $(this).hide();
+    clock();
+});
+
+function clock() {
+    time = setInterval(thirty, 1000);
+    $(".timer").html("Time Left: <br>" + timeCount)
 
 
-$(".startGame").on("click", function (){
-           $(".questionsAndTimer").show();
-            console.log("does the button work?");
-    
-            $(this).hide();
-            clock(); 
-        });
+    function thirty() {
+        if (timeCount === 0) {
+            endScreen();
 
-        function clock() {
-            time = setInterval(thirty, 1000);
-            $(".timer").html("Time Left: <br>" + timeCount)
-
-        
-            function thirty() {
-                if (timeCount === 0) {
-                    timeDone();
-
-                }
-
-                else {
-                    timeCount--;
-                }
-                $(".timer").html("Time Left: <br>" + timeCount)
-            }
         }
 
-// I need to be able to have questions and shit
-//I need to be able to log right and wrong
-// I need an end screen
-// I need a button at the end to reset
-
-
-function timeDone (){
-    
-    $('.questionsAndTimer').hide();
-    //show end screen
+        else {
+            timeCount--;
+        }
+        $(".timer").html("Time Left: <br>" + timeCount)
+    }
 }
+/* Questions Go Here */
+
+
+
+
+
+
+$(".submit").click(function () {
+    endScreen();
+});
+
+
+/* end screen */
+function endScreen() {
+
+    $('.questionsAndTimer').hide();
+    $('.endScreen').show();
+    $("#correct").html("Correct Answers: " + correct);
+    $("#incorrect").html("Incorrect Answers: " + incorrect);
+    $("#unanswered").html("Unanswered: " + unanswered);
+
+}
+
+$(".restart").click(function () {
+    $('.endScreen').hide();
+    $(".startGame").show();
+    reset();
+
+
+});
 
 function reset() {
-    correct= 0;
-    wrong= 0
-    timeCount= 30;
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+    timeCount = 30;
 }
+
+/*
+
+if (correct < 60%){
+    write: YOU SHALL NOT PASS
+    Maybe even due a gif 
+}
+
+*/
