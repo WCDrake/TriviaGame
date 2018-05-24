9ce93e129eac94364f54645de5383a322fa160a3
unknown <wconnordrake@gmail.com>
2018-05-24 17:48:46 -0500

    done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,3 @@
-/* When the clock runs out the scores are not calculated */
-//When the clock runs out, the gif repeats 
-
-
-
 
 var timeCount = 30;
 var correct = 0;
@@ -31,31 +26,37 @@ function clock() {
     time = setInterval(thirty, 1000);
     $(".timer").html("Time Left: <br>" + timeCount)
 
-
     function thirty() {
-        if (timeCount === 0) {
-            /* I need something here */
-            endScreen();
-
+        if (timeCount > 0) {
+            timeCount--
         }
 
-        else {
-            timeCount--;
+        else if (timeCount === 0) {
+            clearInterval(time);
+            endScreen();
         }
         $(".timer").html("Time Left: <br>" + timeCount)
     }
 }
-/* Questions Go Here */
 
-$(".submit").click(function  () {
-   
+$(".submit").click(function () {
+
+
+    endScreen();
+
+});
+
+function endScreen() {
+
+    $('.questionsAndTimer').hide();
+
     var answer1 = document.getElementById("111").checked;
     var answer2 = document.getElementById("65").checked;
     var answer3 = document.getElementById("87").checked;
 
-    if (answer1===true){
+    if (answer1 === true) {
         correct++;
-    } else if (answer2===true || answer3===true) {
+    } else if (answer2 === true || answer3 === true) {
         incorrect++;
     } else {
         unanswered++;
@@ -65,9 +66,9 @@ $(".submit").click(function  () {
     var answer5 = document.getElementById("Boromir").checked;
     var answer6 = document.getElementById("Legolas").checked;
 
-    if (answer4===true){
+    if (answer4 === true) {
         correct++;
-    } else if (answer5===true || answer6===true) {
+    } else if (answer5 === true || answer6 === true) {
         incorrect++;
     } else {
         unanswered++;
@@ -77,54 +78,42 @@ $(".submit").click(function  () {
     var answer8 = document.getElementById("way").checked;
     var answer9 = document.getElementById("totally").checked;
 
-    if (answer8===true){
+    if (answer8 === true) {
         correct++;
-    } else if (answer7===true || answer9===true) {
+    } else if (answer7 === true || answer9 === true) {
         incorrect++;
     } else {
         unanswered++;
     }
-    endScreen();
-
-});
 
 
-
-
-
-
-
-
-
-
-/* end screen */
-
-function endScreen() {
-    $('.questionsAndTimer').hide();
-
-    if (correct > 1){
+    //End Gifs
+    if (correct > 1) {
 
         $('#done').show();
-        
-        setTimeout(function() {
+
+        setTimeout(function () {
             $('#done').hide();
-        },2000);
+        }, 4000);
 
 
     } else {
         $('#noPass').show();
-        setTimeout(function() {
+        setTimeout(function () {
             $('#noPass').hide();
-        },3000);
+        }, 6000);
 
 
     }
-  
+
 
     $('.endScreen').show();
     $("#correct").html("Correct Answers: " + correct);
     $("#incorrect").html("Incorrect Answers: " + incorrect);
     $("#unanswered").html("Unanswered: " + unanswered);
+
+
+
 
 }
 
@@ -135,7 +124,6 @@ $(".restart").click(function () {
     $('#done').hide();
     reset();
 
-
 });
 
 function reset() {
@@ -144,14 +132,16 @@ function reset() {
     unanswered = 0;
     timeCount = 30;
     clearInterval(time);
-   document.getElementById("111").checked = false;
-   document.getElementById("65").checked = false;
-   document.getElementById("87").checked = false;
-   document.getElementById("Frodo").checked = false;
-   document.getElementById("Boromir").checked = false;
-   document.getElementById("Legolas").checked = false;
-   document.getElementById("super").checked = false;
-   document.getElementById("way").checked = false;
-   document.getElementById("totally").checked = false;
+
+    //unclick buttons
+    document.getElementById("111").checked = false;
+    document.getElementById("65").checked = false;
+    document.getElementById("87").checked = false;
+    document.getElementById("Frodo").checked = false;
+    document.getElementById("Boromir").checked = false;
+    document.getElementById("Legolas").checked = false;
+    document.getElementById("super").checked = false;
+    document.getElementById("way").checked = false;
+    document.getElementById("totally").checked = false;
 
 }
